f40f87829a9defa585383ae5b4852e15b4f30bf3
sourinry <144391871+sourinry@users.noreply.github.com>
2025-03-19 12:57:58 +0530

    utils files update

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 export const corsOptions = {
     origin: 'http://localhost:3000',
 };
+
+export const statusCode = {
+    "OK" : 200,
+    "USER ERROR" : 400,
+    "NOT FOUND" : 404,
+    "INTERNAL ERROR" : 500
+}
