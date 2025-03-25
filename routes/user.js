ac89b8f5ea5aa8201407c153ad92ceac0c85457d
sahildadwal20 <sahildadwalei@gmail.com>
2025-03-25 11:41:14 +0530

    new

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ import { verifyAdmin, verifyJWT } from "../middlewares/auth.js";
 const router = express.Router();
 router.use(apiLimiter);
 
-router.get("/", verifyJWT, getUsers);
+router.post("/", verifyJWT, getUsers);
 router.post("/delete", verifyJWT, deleteUser);
 router.post("/register", register);
 router.post("/login", login);
